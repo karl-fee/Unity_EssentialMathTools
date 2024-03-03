25d3d6157b3037d816ed2118fa12f8b49e315f3e
karl-fee <88248364+karl-fee@users.noreply.github.com>
2024-03-03 19:58:09 +0100

    add dot product formula

--- Assets/Code/Scripts/DotProductTool.cs
+++ Assets/Code/Scripts/DotProductTool.cs
@@ -81,4 +81,12 @@ public class DotProductEditor : EditorWindow
         var fmh_51_44_638450909898752133 = Quaternion.identity;
         return Handles.FreeMoveHandle(pos, size, Vector3.zero, Handles.SphereHandleCap);
     }
+
+    float DotProduct(Vector3 p0, Vector3 p1, Vector3 c){
+
+        Vector3 a = (p0 - c).normalized;
+        Vector3 b = (p1 - c).normalized;
+
+        return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
+    }
 }
