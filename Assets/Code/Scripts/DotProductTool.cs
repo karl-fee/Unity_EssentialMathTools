c43532aa8354e2ad484fed8f36a3ce1db3197239
karl-fee <88248364+karl-fee@users.noreply.github.com>
2024-03-03 16:25:29 +0100

    add menu item

--- Assets/Code/Scripts/DotProductTool.cs
+++ Assets/Code/Scripts/DotProductTool.cs
@@ -3,5 +3,9 @@ using UnityEditor;
 
 public class DotProductEditor : EditorWindow
 {
-
+    [MenuItem("Tools/Dot Product")]
+    public static void ShowWindow() {
+        DotProductEditor window = (DotProductEditor) GetWindow(typeof (DotProductEditor), true, "Dot Product");
+        window.Show();
+    }
 }
