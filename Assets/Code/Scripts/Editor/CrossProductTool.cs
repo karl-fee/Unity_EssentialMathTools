a0d6d03a348c80b40ba91b47e7a94ca4165f8f00
karl-fee <88248364+karl-fee@users.noreply.github.com>
2024-03-20 14:22:23 +0100

    add gui style  and cross product method

--- Assets/Code/Scripts/Editor/CrossProductTool.cs
+++ Assets/Code/Scripts/Editor/CrossProductTool.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEditor;
 using UnityEditor.SearchService;
+using UnityEngine.Video;
 
 public class CrossProductTool : CommonEditor, IUpdateSceneGUI
 {
@@ -12,6 +13,7 @@ public class CrossProductTool : CommonEditor, IUpdateSceneGUI
     private SerializedProperty propP;
     private SerializedProperty propQ;
     private SerializedProperty propPXQ;
+    private GUIStyle guiStyle = new GUIStyle();
 
     [MenuItem("Tools/Cross Product Tool")]
     public static void ShowWindow(){
@@ -34,6 +36,10 @@ public class CrossProductTool : CommonEditor, IUpdateSceneGUI
         propQ = obj.FindProperty("m_q");
         propPXQ = obj.FindProperty("m_pxq");
 
+        guiStyle.fontSize = 25;
+        guiStyle.fontStyle = FontStyle.Bold;
+        guiStyle.normal.textColor = Color.white;
+
         SceneView.duringSceneGui -= SceneGUI;
     }
 
@@ -42,10 +48,41 @@ public class CrossProductTool : CommonEditor, IUpdateSceneGUI
     }
 
     private void OnGUI(){
+        obj.Update();
+        DrawBlockGUI("P", propP);
+        DrawBlockGUI("Q", propQ);
+        DrawBlockGUI("PXQ", propPXQ);
+
+        if(obj.ApplyModifiedProperties()){
+            SceneView.RepaintAll();
+        }
+        
+        if(GUILayout.Button("Reset Values")){
+            SetDefaultValues();
+        }
 
     }
 
     public void SceneGUI(SceneView view){
         throw new System.NotImplementedException();
     }
+
+    private void DrawLineGUI(Vector3 pos, string tex, Color col){
+        Handles.color = col;
+        Handles.Label(pos, tex, guiStyle);
+        Handles.DrawAAPolyLine(3f, pos, Vector3.zero);
+    }
+
+    private void RepaintOnGUI(){
+        Repaint();
+    }
+
+    Vector3 CrossProduct(Vector3 p, Vector3 q){
+        float x = p.y * q.z - p.z * q.y;
+        float y = p.z * q.x - p.x * q.z;
+        float z = p.x * q.y - p.y * q.x;
+
+        return new Vector3(x, y, z);
+    }
+
 }
